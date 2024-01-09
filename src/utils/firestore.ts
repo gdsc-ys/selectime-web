c6a70f29625dbcd5c8fc77babb73dfa2d336f73c
namooplus <namoo.developer@gmail.com>
2024-01-09 21:46:57 +0900

    feat: add env

--- src/utils/firestore.ts
+++ src/utils/firestore.ts
@@ -6,9 +6,15 @@ import {
   collection,
   getFirestore,
 } from "firebase/firestore";
-import config from "firebase-config.json";
 
-const app = initializeApp(config);
+const app = initializeApp({
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+});
 const db = getFirestore(app);
 
 /**
